bea587cb56337994435b995743d24ea3888bdeac
ragava <raghavareddypathi@gmail.com>
2025-10-07 22:24:16 +0530

    debugging

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -31,19 +31,25 @@ async function authUserMiddleware(req, res, next) {
     console.log('checking auth user middleware')
     const token = req.cookies.token
     if(!token){
+        console.log('no token')
         return res.status(401).json({
             message:'please login first'
         })
     }
 
     try {
+        console.log('token verification started')
         const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        console.log('finding user in db')
         const user = await userModel.findById(decoded._id)
+        console.log('found user in db')
     
         req.user = user
+        console.log('token verification done')
         next()
         
     } catch (error) {
+        console.log('token verification failed')
         console.log(error)
         res.status(401).json({
             message:'Invalid user '
